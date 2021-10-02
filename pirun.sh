f2f358b1da007ee392d066ba9dc4299338873ed1
amirma <amirma@radware.com>
2021-10-02 16:54:41 +0300

    compose

--- pirun.sh
+++ pirun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker-compose down
-docker-compose -f redis.yaml -d up
+docker-compose -f ./redis.yaml down
+docker-compose -f ./redis.yaml -d up
 
 cd backend
 kill -9 $(lsof -t -i:1323)
