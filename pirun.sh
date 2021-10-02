76be065e0562d5c66615f0ebfee9472851823da0
amirma <amirma@radware.com>
2021-10-02 21:44:17 +0300

    update

--- pirun.sh
+++ pirun.sh
@@ -3,7 +3,8 @@
 docker-compose -f ./redis.yaml down
 docker-compose -f ./redis.yaml up -d
 
-cd backend
+cd backend || exit
+# shellcheck disable=SC2046
 kill -9 $(lsof -t -i:1323)
 sudo rmmod uvcvideo
 sudo modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80
