a9b747cd2a59d33d749f01a20f9913d5ecbc19f8
amirma <amirma@radware.com>
2021-10-23 13:36:11 +0300

    streamer

---
+++ backend/streamer/streamer.go
@@ -0,0 +1,39 @@
+package streamer
+
+import (
+	"net/http"
+	"strconv"
+	"www.seawise.com/shrimps/backend/capture"
+	"www.seawise.com/shrimps/backend/log"
+)
+
+type Streamer struct {
+	Client *http.ServeMux
+	Capt   *capture.Capture
+}
+
+func Create(capt *capture.Capture) *Streamer {
+	s := http.NewServeMux()
+
+	return &Streamer{
+		Client: s,
+		Capt: capt,
+	}
+}
+
+func (s *Streamer) Produce(){
+	for i:=0; i < len(s.Capt.Channels); i++ {
+		path := "/stream/" + strconv.Itoa(i)
+		log.V5("PATH:", string(path))
+		s.Client.Handle(path, s.Capt.Channels[i].Stream)
+	}
+}
+
+func (s *Streamer) Start() {
+	err := http.ListenAndServe(":8080", s.Client)
+	if err != nil {
+		panic(err)
+	}
+}
+
+
