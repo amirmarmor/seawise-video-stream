66a543e0f599270be8c0ab1e297949337cf8f2c8
amirma <amirma@radware.com>
2021-11-02 18:56:13 +0200

    new update mechanism

--- pirun.sh
+++ pirun.sh
@@ -2,11 +2,11 @@
 
 cd backend || exit
 
-sudo rmmod uvcvideo
-sudo modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80
-
+#sudo rmmod uvcvideo
+#sudo modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80
+#
 export VERBOSE=5
-export APIHOST="seawisely.com"
+export APIHOST=""
 echo $APIHOST
 cmd=./start
 $cmd
